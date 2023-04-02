c6cdb4d44e345cc00d1bf0ce705e1e3018650245
saiderrahmani <didi.mousedidi@outlook.com>
2023-04-02 00:41:19 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+// Code your solution hereconst drivers = [
+  
+ 
+function findMatching(drivers, name) { 
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+
+ const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+console.log(findMatching(drivers, 'a')); // ['sammy', 'annette', 'sarah']
+console.log(findMatching(drivers, 'B')); // ['bobby']
+console.log(findMatching(drivers, 'x')); // []
+
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+  }
+    
